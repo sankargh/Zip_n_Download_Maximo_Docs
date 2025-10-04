7f31594b79a15440a5cd2b32ab3576323daa024e
sankargh <99525703+sankargh@users.noreply.github.com>
2025-10-04 17:52:08 +0800

    Update Compress_n_Download_Maximo_Docs.py
    Added comments

--- Compress_n_Download_Maximo_Docs.py
+++ Compress_n_Download_Maximo_Docs.py
@@ -8,20 +8,28 @@ from java.util.zip import ZipEntry;
 from java.io import File;
 
 if launchPoint=='<ActionName>':
+    #Get the dialog bean
     session=service.webclientsession()
     docTable=session.getDataBean("<dialog_id>")
+    
+    #Get the selection of files/attachments from dialog
     docVector=docTable.getSelection()
     
+    #Add the file paths/doclink URLs to an array
     filePaths=[]
     for doc in docVector:
         file=doc.getString("URLNAME")
         filePaths.append(file)
     
+    #Set the download folder, filenames
     downLoadPath="C:\\Users\\Public\\Downloads\\";
     fullPath=downLoadPath+"<FileName>.zip";
+    
+    #Initiate the file, zip-file output stream
     fos = FileOutputStream(fullPath);
     zipOut = ZipOutputStream(fos);
-  
+    
+    #Read each file and write into zip file.
     try:
         for filePath in filePaths:
             fileToZip = File(filePath);
